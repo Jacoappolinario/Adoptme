0032736cc0ef79a8a1cd3db2ade48027e3148035
Jacó Apolinário <jacoapollinario@gmail.com>
2021-02-23 11:54:17 -0300

    add create user models and password encryption

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 const db = require('../../config/db')
+const { hash } = require('bcryptjs')
 
 module.exports = {
     async findOne(filters) {
@@ -18,5 +19,32 @@ module.exports = {
 
         const results = await db.query(query)
         return results.rows[0]
+    },
+    async create(data) {
+        try {
+            const query = `
+                INSERT INTO users (
+                    name,
+                    email,
+                    password
+                ) VALUES ($1, $2, $3)
+                RETURNING id
+            `
+
+            // hash of password
+            const passwordHash = await hash(data.password, 8)
+
+            const values = [
+                data.name,
+                data.email,
+                passwordHash
+            ]
+
+            const results = await db.query(query, values)
+            return results.rows[0].id
+
+        } catch(err) {
+            console.error(err)
+        }
     }
 }
