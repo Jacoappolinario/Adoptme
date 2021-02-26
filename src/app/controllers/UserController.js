bce2ab8f18b33f5e59ecf5b1a6c9a2bdcc8584e4
Jacó Apolinário <jacoapollinario@gmail.com>
2021-02-26 13:22:04 -0300

    add user update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,14 +5,8 @@ module.exports = {
         return res.render("user/register")
     },
     async show(req, res) {
-        const { userId: id } = req.session
-
-        const user = await User.findOne({ where: {id} })
-
-        if (!user) return res.render("user/register", {
-            error: "Usuário não encontrado!"
-        })
-
+        const { user } = req
+        
         return res.render('user/index', { user })
     },
     async post(req, res) {
@@ -22,5 +16,28 @@ module.exports = {
         req.session.userId = userId
 
         return res.redirect('/users')
+    },
+    async update(req, res) {
+        try {
+            const { user } = req
+
+            let { name, email } = req.body 
+
+            await User.update(user.id, {
+                name,
+                email
+            })
+
+            return res.render("user/index", {
+                user: req.body,
+                success: "Conta atualizada com sucesso!"
+            })
+
+        } catch(err) {
+            console.error(err)
+            return res.render("user/index", {
+                error: "Algum erro aconteceu!"
+            })
+        }
     }
 }
